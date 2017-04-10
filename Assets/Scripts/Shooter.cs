8678212a413276c22a06bbcf5546390e279cefe1
TakaoWing <tt.wing2838783@gmail.com>
2017-04-11 02:42:01 +0900

    create Shooter.cs < SampleCandy

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour {
 
-	public GameObject candyPrefab;
+	const int SphereCandyFrequency = 3;
+
+	int sampleCandyCount;
+
+	public GameObject[] candyPrefabs;
+	public GameObject[] candySquarePrefabs;
 	public float shotSpeed;
 	public float shotTorque;
+	public float baseWidth;
 
 	void Start () {
 		
@@ -17,9 +23,27 @@ public class Shooter : MonoBehaviour {
 			Shot ();
 	}
 
+	GameObject SampleCandy(){
+		
+		GameObject prefab = null;
+
+		// 特定の回数に一回丸いCandyを選択
+		if (sampleCandyCount % SphereCandyFrequency == 0) {
+			int index = Random.Range (0, candyPrefabs.Length);
+			prefab = candyPrefabs [index];
+		} else {
+			int index = Random.Range (0, candySquarePrefabs.Length);
+			prefab = candyPrefabs [index];
+		}
+
+		sampleCandyCount++;
+
+		return prefab;
+	}
+
 	public void Shot(){
 		// プレファブからCandyオブジェクトを生成
-		GameObject candy = Instantiate(candyPrefab, transform.position, Quaternion.identity) as GameObject; // オブジェクトの生成
+		GameObject candy = Instantiate(SampleCandy(), transform.position, Quaternion.identity) as GameObject; // オブジェクトの生成
 
 		// CadnyオブジェクトのRigidbodyを取得し力と回転を加える
 		Rigidbody candyRigidBody = candy.GetComponent<Rigidbody>();
