27af04faa773a710111de927c5dd2f04ab647540
TakaoWing <tt.wing2838783@gmail.com>
2017-04-11 03:39:27 +0900

    update CandyDestroyer.cs

--- Assets/CandyDestroyer.cs
+++ Assets/CandyDestroyer.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class CandyDestroyer : MonoBehaviour {
 
+	public CandyHolder candyHolder;
+	public int reward;
+
 
 	void OnTriggerEnter(Collider other){ // 衝突したとき呼ばれる
-		if (other.gameObject.tag == "Candy")
+		if (other.gameObject.tag == "Candy"){
+			candyHolder.AddCandy (reward); // 指定数だけCandyのストックを増やすs
 			Destroy (other.gameObject); // オブジェクトの削除
+		}
 	}
 
 }
