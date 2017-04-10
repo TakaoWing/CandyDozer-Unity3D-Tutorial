98b3b04fb4b4fe55f8421720f56df2c267116940
TakaoWing <tt.wing2838783@gmail.com>
2017-04-11 03:10:11 +0900

    create CandyHolder.cs

--- Assets/CandyDestroyer.cs
+++ Assets/CandyDestroyer.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class CandyDestroyer : MonoBehaviour {
 
-	void Start () {
-		
+
+	void OnTriggerEnter(Collider other){ // 衝突したとき呼ばれる
+		if (other.gameObject.tag == "Candy")
+			Destroy (other.gameObject); // オブジェクトの削除
 	}
 
-	void Update () {
-		
-	}
 }
