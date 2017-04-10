2ff80a44f772bd387dbdf0fa0eab14e77c859748
TakaoWing <tt.wing2838783@gmail.com>
2017-04-11 02:33:30 +0900

    update Shooter.cs

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+
+	public GameObject candyPrefab;
+	public float shotSpeed;
+	public float shotTorque;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		if (Input.GetButtonDown ("Fire1"))
+			Shot ();
+	}
+
+	public void Shot(){
+		// プレファブからCandyオブジェクトを生成
+		GameObject candy = Instantiate(candyPrefab, transform.position, Quaternion.identity) as GameObject; // オブジェクトの生成
+
+		// CadnyオブジェクトのRigidbodyを取得し力と回転を加える
+		Rigidbody candyRigidBody = candy.GetComponent<Rigidbody>();
+		candyRigidBody.AddForce (transform.forward * shotSpeed);
+		candyRigidBody.AddTorque(new Vector3(0,shotTorque,0));
+	}
+}
