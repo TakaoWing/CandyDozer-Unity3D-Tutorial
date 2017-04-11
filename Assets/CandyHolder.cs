338219866b21f1f7483527fccd7c895cee198a67
TakaoWing <tt.wing2838783@gmail.com>
2017-04-11 14:22:54 +0900

    update CandyHolder.cs < RecaverryCandy

--- Assets/CandyHolder.cs
+++ Assets/CandyHolder.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CandyHolder : MonoBehaviour {
 
-	const int DefaultCandyAmount = 30;
+	const int DefaultCandyAmount = 30; // キャンディのストックの初期値
+	const int RecoverSeconds = 10; // ストック回復の初期値
 
 	// 現在のキャンディのストック数
 	int candy = DefaultCandyAmount;
+	// ストック回復までの残り秒数
+	int counter;
 
 	public void ConsumeCandy(){ // キャンディを減らす
 		if(candy > 0) candy--;
@@ -27,6 +30,27 @@ public class CandyHolder : MonoBehaviour {
 		// キャンディのストック数を表示
 		string label = "Candy : " + candy;
 
-		GUI.Label (new Rect (0, 0, 100, 30), label);
+		// 回復カウントしている時だけ秒数を表示
+		if (counter > 0) label = label +  " ( " + counter + " s ) ";
+
+		GUI.Label (new Rect (0, 0, 120, 30), label);
+	}
+
+	void Update(){
+		// キャンディのストックがデフォルトより少なく
+		// 回復カウントをしていないときにカウントをスタートさせる
+		if(candy < DefaultCandyAmount && counter <= 0) StartCoroutine(RecoverCandy());
+	}
+
+	IEnumerator RecoverCandy(){
+		counter = RecoverSeconds;
+
+		// １秒ずつカウントを進める
+		while (counter > 0) {
+			yield return new WaitForSeconds(1.0f);
+			counter--;
+		}
+
+		candy++;
 	}
 }
