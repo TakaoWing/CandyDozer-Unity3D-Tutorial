08461ff8f96f627805566841752bf0624fe33f1f
TakaoWing <tt.wing2838783@gmail.com>
2017-04-11 15:50:35 +0900

    create Candy get PGM

--- Assets/CandyDestroyer.cs
+++ Assets/CandyDestroyer.cs
@@ -6,12 +6,22 @@ public class CandyDestroyer : MonoBehaviour {
 
 	public CandyHolder candyHolder;
 	public int reward;
+	public GameObject effectPrefab; // エフェクトプレファブ
+	public Vector3 effectRotation; // エフェクトローテーション
 
 
 	void OnTriggerEnter(Collider other){ // 衝突したとき呼ばれる
+		
 		if (other.gameObject.tag == "Candy"){
-			candyHolder.AddCandy (reward); // 指定数だけCandyのストックを増やすs
+			
+			candyHolder.AddCandy (reward); // 指定数だけCandyのストックを増やす
+
 			Destroy (other.gameObject); // オブジェクトの削除
+
+			if (effectPrefab != null) {
+				// Candyのポジションにエフェクトを生成
+				Instantiate(effectPrefab, other.transform.position, Quaternion.Euler(effectRotation));
+			}
 		}
 	}
 
